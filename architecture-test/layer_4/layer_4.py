938708be494d7008ac32bd6da13a49a2c3bf12c5
Ilkay Tevfik Devran <devrani@mef.edu.tr>
2018-09-19 23:37:33 +0300

    architecture-test layer_4 script error fixed

--- architecture-test/layer_4/layer_4.py
+++ architecture-test/layer_4/layer_4.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 import requests
-from layer_3_dataBase import DataBase as DB
+from layer_4_dataBase import DataBase as DB
 from multiprocessing import Process
 import boto3
 import json
@@ -11,12 +11,17 @@ def main(event, context):
 
     time.sleep(event['delay'])
     
-    url = 'https://api.ipify.org/?format=json'
-    r = requests.get(url)
+    try:
+        url = 'https://api.ipify.org/?format=json'
+        r = requests.get(url)
 
-    ip = json.loads(r.text)['ip']
-    layer = event['layer']
-    number = event['number']
+        ip = json.loads(r.text)['ip']
+        layer = event['layer']
+        number = event['number']
+    except:
+        ip = 'null'
+        layer = event['layer']
+        number = event['number']
 
     db = DB('innodb')
     db.insert(ip,layer,number)
